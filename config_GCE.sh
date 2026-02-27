6a1cac781a7fb199e17c35cda667d6b6b77912ac
Joseph <josephelforzli@gmail.com>
2026-02-27 15:57:22 +0100

    config gce

--- config_GCE.sh
+++ config_GCE.sh
@@ -2,10 +2,11 @@
 USERNAME=ubuntu
 
 # IP of the EC2 instance
-IP=16.171.162.182
+IP=34.133.176.120
 
 # SSH key file
-SSH_FILE=~/.ssh/aws-rene.pem
+SSH_FILE=~/.ssh/gce_rene
+#aws-rene.pem
 
 # Git-Hub repo of the project
 GIT_HUB_repo_of_the_project=https://github.com/joseph-elf/Rene.git
